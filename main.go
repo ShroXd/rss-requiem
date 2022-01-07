4ddd9235159e1411bcf5cb088eda052c0cb2cd28
ShroXd <ShroXdF@gmail.com>
2022-01-07 22:04:45 +0800

    refactor: 💡 refactor the base code

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ import (
 )
 
 func main() {
-	p := CreateParser()
-	p.FetchURL("https://news.ycombinator.com/rss")
+	bp := BaseParser()
+	ro := createRequestOptions(RequestOptions{
+		ProxyURL: "http://localhost:4780",
+		Timeout:  6 * time.Second,
+	})
+
+	bp.FetchURL("https://news.ycombinator.com/rss", ro)
 
 	// fp := gofeed.NewParser()
 	// feed, _ := fp.ParseURL("https://feeds.feedburner.com/ruanyifeng")
@@ -23,32 +28,73 @@ type Parser struct {
 	UserAgent string
 }
 type Feed struct{}
+type RequestOptions struct {
+	UserAgent string
+	ProxyURL  string
+	Timeout   time.Duration
+}
 
-func CreateParser() *Parser {
-	p := Parser{
+func createRequestOptions(o RequestOptions) RequestOptions {
+	options := RequestOptions{
 		UserAgent: "RssRequiem",
 	}
 
-	return &p
+	if o.ProxyURL != "" {
+		options.ProxyURL = o.ProxyURL
+	}
+	if o.Timeout != 0 {
+		options.Timeout = o.Timeout
+	}
+
+	return options
 }
 
-func (p *Parser) FetchURL(feedURL string) (feed *Feed, err error) {
-	req, err := http.NewRequest("GET", feedURL, nil)
+func createProxy(proxyURL string) http.Transport {
+	URL, err := url.Parse(proxyURL)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	return http.Transport{
+		Proxy: http.ProxyURL(URL),
+	}
+
+}
+
+func createClient(options RequestOptions) http.Client {
+	transport := createProxy(options.ProxyURL)
+
+	return http.Client{
+		Timeout:   options.Timeout,
+		Transport: &transport,
+	}
+}
+
+func createRequest(requestURL string, options RequestOptions) (request *http.Request, err error) {
+	req, err := http.NewRequest("GET", requestURL, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	req = req.WithContext(context.Background())
-	req.Header.Set("User-Agent", p.UserAgent)
+	req.Header.Set("User-Agent", options.UserAgent)
 
-	proxyURL, err := url.Parse("http://localhost:4780")
-	transport := http.Transport{
-		Proxy: http.ProxyURL(proxyURL),
+	return req, nil
+}
+
+func BaseParser() *Parser {
+	p := Parser{}
+
+	return &p
+}
+
+func (p *Parser) FetchURL(feedURL string, options RequestOptions) (feed *Feed, err error) {
+	req, err := createRequest(feedURL, options)
+	if err != nil {
+		fmt.Print(err)
 	}
-	client := http.Client{
-		Timeout:   6 * time.Second,
-		Transport: &transport,
-	}
+
+	client := createClient(options)
 
 	res, err := client.Do(req)
 	if err != nil {
